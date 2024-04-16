340b6f4b92b817b926dfb53e6995c076cd912350
Kevin Li <kjli@wisc.edu>
2024-04-16 18:38:26 -0500

    Update README, Remove Unnecessary Comments and Files
    We can submit, but we may want to continue looking for ways to improve time efficiency to ensure consistency for test 6.

--- p6/ring_buffer.c
+++ p6/ring_buffer.c
@@ -7,7 +7,6 @@
 
 int init_ring(struct ring *r) {
     if (r == NULL) return -1;
-    // memset(r, 0, sizeof(struct ring));
     // Initialize semaphores for shared usage
     sem_init(&r->sem_not_full, 1, RING_SIZE);  // Initial value is the size of the ring
     sem_init(&r->sem_not_empty, 1, 0);         // Initial value is 0
@@ -29,9 +28,7 @@ void ring_submit(struct ring *r, struct buffer_descriptor *bd) {
 
 void ring_get(struct ring *r, struct buffer_descriptor *bd) {
     if (r == NULL || bd == NULL) return;
-    //printf("before wait\n");
     sem_wait(&r->sem_not_empty);
-    //printf("ring get\n");
     pthread_mutex_lock(&r->g_mutex);
     *bd = r->buffer[r->c_tail];
     r->c_tail = (r->c_tail + 1) % RING_SIZE;
