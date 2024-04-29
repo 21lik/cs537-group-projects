e637c56b878ea33b3e83006a9a77989eea5ab1bd
Xuyang Liu <xuyang@royal-23.cs.wisc.edu>
2024-04-29 13:42:44 -0500

    edit mkfs.c and it can pass test 0 now

--- p7/mkfs.c
+++ p7/mkfs.c
@@ -15,32 +15,32 @@ int main(int argc, char *argv[]) {
     // TODO: test
     char *d = NULL;
     int i = 0, b = 0;
-    for (int j = 0; j < argc; j++) {
+    for (int j = 1; j < argc; j+=2) {
         if (strcmp(argv[j], "-d") == 0)
-            d = argv[++j];
+            d = argv[j+1];
         else if (strcmp(argv[j], "-i") == 0)
-            i = atoi(argv[++j]);
+            i = atoi(argv[j+1]);
         else if (strcmp(argv[j], "-b") == 0)
-            b = atoi(argv[++j]);
+            b = atoi(argv[j+1]);
     }
 
     // Ensure at least one inode and data block exist for root directory
-    if (i <= 0 || d <= 0) {
+    if (!d || i <= 0 || b <= 0) {
         printf("Must have at least one inode and data block for root directory\n");
-        exit(1); // TODO: return or exit?
+        return 1; // TODO: return or exit?
     }
 
     // Round number of blocks up to nearest multiple of 32
     if (i % 32 != 0)
-        i += 32 - (i & 31);
+        i = i + 32 - (i % 31);
     if (b % 32 != 0)
-        b += 32 - (b & 31);
+        b = b + 32 - (b % 31);
     
     printf("inode count: %d\ndata block count: %d\n", i, b); // TODO: debug
 
     // Open disk image file, mmap onto memory
     int fd = open(d, O_RDWR);
-    off_t sb_size = BLOCK_SIZE, ibitmap_size = i >> 3, dbitmap_size = b >> 3, inodes_size = i * BLOCK_SIZE, data_blocks_size = b * BLOCK_SIZE; // Use bit operations for bitmaps
+    off_t sb_size = sizeof(struct wfs_sb), ibitmap_size = i / 8, dbitmap_size = b / 8, inodes_size = i * BLOCK_SIZE, data_blocks_size = b * BLOCK_SIZE; // Use bit operations for bitmaps
     size_t filesystem_size = sb_size + ibitmap_size + dbitmap_size + inodes_size + data_blocks_size;
     struct stat statbuf;
     fstat(fd, &statbuf);
@@ -49,20 +49,20 @@ int main(int argc, char *argv[]) {
     printf("disk image size: %ld\n", disk_img_size); // TODO: debug
     if (disk_img_size < filesystem_size) {
         printf("Error: disk image file too small to accomodate number of blocks\n");
-        exit(1); // TODO: return or exit?
+        return 1; // TODO: return or exit?
     }
 
-    void *addr = mmap(NULL, filesystem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    char *addr = mmap(NULL, filesystem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     close(fd);
 
     if (addr == MAP_FAILED) {
-        exit(1); // TODO: return or exit?
+        return 1; // TODO: return or exit?
     }
 
     memset(addr, 0, filesystem_size); // Clear disk image file // TODO: is this necessary?
 
     // Write superblock
-    struct wfs_sb *sb = addr;
+    struct wfs_sb *sb = (struct wfs_sb *) addr;
     sb->num_inodes = i;
     sb->num_data_blocks = b;
     sb->i_bitmap_ptr = sb_size;
@@ -71,7 +71,7 @@ int main(int argc, char *argv[]) {
     sb->d_blocks_ptr = sb->i_blocks_ptr + inodes_size;
 
     // Write root inode
-    struct wfs_inode *root_inode = (struct wfs_inode*) (((char*) sb) + sb->i_blocks_ptr); // TODO: implement allocate_inode() (in shared file, so wfs.c can use as well), make sure it looks for inode numbers in order (for this one, must get inode 0)
+    struct wfs_inode *root_inode = (struct wfs_inode*) (sb + sb->i_blocks_ptr); // TODO: implement allocate_inode() (in shared file, so wfs.c can use as well), make sure it looks for inode numbers in order (for this one, must get inode 0)
     root_inode->num = 0;
     root_inode->mode = S_IFDIR | S_IRWXU | S_IRWXG | S_IRWXO;
     root_inode->uid = getuid();
