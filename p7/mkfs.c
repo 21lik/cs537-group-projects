9edd5483d2dceced2b393aaa90df8f0b2a29c88e
Xuyang Liu <xuyang@royal-30.cs.wisc.edu>
2024-04-30 20:10:17 -0500

    update mkfs.c

--- p7/mkfs.c
+++ p7/mkfs.c
@@ -13,21 +13,30 @@
 */
 int main(int argc, char *argv[]) {
     // TODO: test
+    if (argc < 7) {
+        fprintf(stderr, "Usage: %s -d <disk_img> -i <num_inodes> -b <num_blocks>\n", argv[0]);
+        return 1;
+    }
+
     char *d = NULL;
     int i = 0, b = 0;
-    for (int j = 1; j < argc; j+=2) {
-        if (strcmp(argv[j], "-d") == 0)
+
+    for (int j = 1; j < argc; j++) {
+        if (strcmp(argv[j], "-d") == 0){
             d = argv[j+1];
-        else if (strcmp(argv[j], "-i") == 0)
+        }
+        else if (strcmp(argv[j], "-i") == 0){
             i = atoi(argv[j+1]);
-        else if (strcmp(argv[j], "-b") == 0)
+        }
+        else if (strcmp(argv[j], "-b") == 0){
             b = atoi(argv[j+1]);
+        }
     }
 
     // Ensure at least one inode and data block exist for root directory
     if (!d || i <= 0 || b <= 0) {
         printf("Must have at least one inode and data block for root directory\n");
-        return 1; // TODO: return or exit?
+        return 1;
     }
 
     // Round number of blocks up to nearest multiple of 32
@@ -35,17 +44,19 @@ int main(int argc, char *argv[]) {
         i = i + 32 - (i % 32);
     if (b % 32 != 0)
         b = b + 32 - (b % 32);
-    
+
     printf("inode count: %d\ndata block count: %d\n", i, b); // TODO: debug
 
     // Open disk image file, mmap onto memory
-    int fd = open(d, O_RDWR);
-    off_t sb_size = sizeof(struct wfs_sb);
+    int fd = open(d, O_RDWR | O_CREAT, 0666);
+    size_t sb_size = sizeof(struct wfs_sb);
     size_t ibitmap_size = (i + 7) / 8;
     size_t dbitmap_size = (b + 7) / 8;
     size_t inodes_size = i * BLOCK_SIZE;
     size_t data_blocks_size = b * BLOCK_SIZE; // Use bit operations for bitmaps
     size_t filesystem_size = sb_size + ibitmap_size + dbitmap_size + inodes_size + data_blocks_size;
+
+
     struct stat statbuf;
     fstat(fd, &statbuf);
     size_t disk_img_size = statbuf.st_size;
@@ -70,7 +81,7 @@ int main(int argc, char *argv[]) {
     sb->num_inodes = i;
     sb->num_data_blocks = b;
     sb->i_bitmap_ptr = sb_size;
-    sb->d_bitmap_ptr = sb_size + ibitmap_size;
+    sb->d_bitmap_ptr = sb->i_bitmap_ptr + ibitmap_size;
     sb->i_blocks_ptr = sb->d_bitmap_ptr + dbitmap_size;
     sb->d_blocks_ptr = sb->i_blocks_ptr + inodes_size;
 
@@ -86,7 +97,9 @@ int main(int argc, char *argv[]) {
     root_inode->atim = curr_time;
     root_inode->mtim = curr_time;
     root_inode->ctim = curr_time;
-    *(((char*) sb) + sb->i_bitmap_ptr) = (char) 128; // 0b10000000
+    root_inode->blocks[0] = sb->d_blocks_ptr; // 0b10000000
+    ((char *)(addr + sb->i_bitmap_ptr))[0] |= 0x01; // Root inode bitmap
+    ((char *)(addr + sb->d_bitmap_ptr))[0] |= 0x01; // Root block bitmap
 
     // Free memory space
     munmap(addr, filesystem_size);
